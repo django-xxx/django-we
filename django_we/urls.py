1f561e8fe9b9c752d56d5b8fdb3e70ad0d787ad8
Brightcells <brightcells@gmail.com>
2017-09-06 18:49:09 +0800

    :sparkles: Add new url urlpatterns

--- django_we/urls.py
+++ django_we/urls.py
@@ -6,6 +6,9 @@ from django_we import views as we_views
 
 # WeChat OAuth2
 urlpatterns = [
+    url(r'^o$', we_views.we_oauth2, name='o'),
+    url(r'^oauth$', we_views.we_oauth2, name='oauth'),
+    url(r'^oauth2$', we_views.we_oauth2, name='oauth2'),
     url(r'^we_oauth2$', we_views.we_oauth2, name='we_oauth2'),
     url(r'^base_redirect$', we_views.base_redirect, name='base_redirect'),
     url(r'^userinfo_redirect$', we_views.userinfo_redirect, name='userinfo_redirect'),
